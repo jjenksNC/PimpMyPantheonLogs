012628cb2103415f3b516bcdd6f9d026765b2643
Joshua Jenks <jjenks@wearenolte.com>
2019-06-12 06:16:06 -0400

    updated site-logs.sh to create directory for site dynamically and then populate the logs in the proper place.

--- site-logs.sh
+++ site-logs.sh
@@ -1,9 +1,12 @@
 # Site UUID from Dashboard URL, eg 12345678-1234-1234-abcd-0123456789ab
-SITE_UUID=
-ENV=
+
+read -p "Enter the site-slug: " SITE_SLUG
+SITE_UUID=$(terminus site:lookup $SITE_SLUG)
+`mkdir $SITE_SLUG`
+read -p "Enter the environment: " ENV
 for app_server in `dig +short appserver.$ENV.$SITE_UUID.drush.in`;
 do
-  rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV.$SITE_UUID@appserver.$ENV.$SITE_UUID.drush.in:logs/* app_server_$app_server
+  rsync -rlvz --size-only --ipv4 --progress -e 'ssh -p 2222' $ENV.$SITE_UUID@appserver.$ENV.$SITE_UUID.drush.in:logs/* $SITE_SLUG/app_server_$app_server
 done
 
 # Include MySQL logs
